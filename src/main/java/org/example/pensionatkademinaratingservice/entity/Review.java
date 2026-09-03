c55948c2dd81bf93b70ada4b33e31549f77397aa
Simon-Fredholm <simon.fredholm2@gmail.com>
2026-09-03 13:41:52 +0200

    omgjord entity, restored

--- src/main/java/org/example/pensionatkademinaratingservice/entity/Review.java
+++ src/main/java/org/example/pensionatkademinaratingservice/entity/Review.java
@@ -1,1 +1,82 @@
-git
+package org.example.pensionatkademinaratingservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long customerId;
+    private Long roomId;
+    private int rating;
+    private String comment;
+    private LocalDate date;
+
+    public Review() {
+    }
+
+    public Review(Long id, Long customerId, Long roomId, int rating, String comment, LocalDate date) {
+        this.id = id;
+        this.customerId = customerId;
+        this.roomId = roomId;
+        this.rating = rating;
+        this.comment = comment;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
